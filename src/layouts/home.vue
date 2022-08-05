7ea41299312408809e94137a49d4cce3c7567798
Daniel Ahn <et3rnal.d@gmail.com>
2022-08-05 10:18:26 -0400

    fix renaming project folder bug, ensure text is black

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- control bg-color here -->
-  <main class="bg-[#F2F2F2] text-gray-700 dark:text-gray-200">
+  <main class="bg-[#F2F2F2] text-black">
     <Header />
     <RouterView />
 
